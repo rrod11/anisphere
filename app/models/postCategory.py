8cb3727ad9a4dda0eb7447261830523fa3d8deb0
Roderick <Roderickj800@gmail.com>
2023-12-04 18:07:14 -0500

    troubleshooting

--- app/models/postCategory.py
+++ app/models/postCategory.py
@@ -1,22 +1,22 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+# from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
-class PostCategory(db.Model):
-    __tablename__ = 'postCategories'
+# class PostCategory(db.Model):
+#     __tablename__ = 'postCategories'
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+#     if environment == "production":
+#         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, primary_key=True)
-    postId = db.Column(db.Integer())
-    categoryId = db.Column(db.Integer(), nullable=False)
-
-
+#     id = db.Column(db.Integer, primary_key=True)
+#     postId = db.Column(db.Integer())
+#     categoryId = db.Column(db.Integer(), nullable=False)
 
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'postId': self.postId,
-            'categoryId': self.categoryId,
-        }
+
+
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'postId': self.postId,
+#             'categoryId': self.categoryId,
+#         }
--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
+def seed_posts():
     seed_post1= Post(title='The Time I Got Reincarnated as A Slime', description="Lonely thirty-seven-year-old Satoru Mikami is stuck in a dead-end job, unhappy with his mundane life, but after dying at the hands of a robber, they awaken to a fresh start in a fantasy realm... as a slime! As Rimuru acclimates to their new, goopy, existence, their exploits with the other monsters set off a chain of events that will change the world forever!", userId="2", categoriesId="1" )
     seed_post2 = Post(title='Black Clover', description="Asta and Yuno were abandoned at the same church on the same day. Raised together as children, they came to know of the 'Wizard King'—a title given to the strongest mage in the kingdom—and promised that they would compete against each other for the position of the next Wizard King. However, as they grew up, the stark difference between them became evident. While Yuno is able to wield magic with amazing power and control, Asta cannot use magic at all and desperately tries to awaken his powers by training physically. When they reach the age of 15, Yuno is bestowed a spectacular Grimoire with a four-leaf clover, while Asta receives nothing. However, soon after, Yuno is attacked by a person named Lebuty, whose main purpose is to obtain Yuno's Grimoire. Asta tries to fight Lebuty, but he is outmatched. Though without hope and on the brink of defeat, he finds the strength to continue when he hears Yuno's voice. Unleashing his inner emotions in a rage, Asta receives a five-leaf clover Grimoire, a 'Black Clover' giving him enough power to defeat Lebuty. A few days later, the two friends head out into the world, both seeking the same goal—to become the Wizard King!", userId="1", categoriesId="2" )
     seed_post3 = Post(title='Mushoku Tensei: Jobless Reincarnation', description="Despite being bullied, scorned, and oppressed all of his life, a 34-year-old shut-in still found the resolve to attempt something heroic—only for it to end in a tragic accident. But in a twist of fate, he awakens in another world as Rudeus Greyrat, starting life again as a baby born to two loving parents. Preserving his memories and knowledge from his previous life, Rudeus quickly adapts to his new environment. With the mind of a grown adult, he starts to display magical talent that exceeds all expectations, honing his skill with the help of a mage named Roxy Migurdia. Rudeus learns swordplay from his father, Paul, and meets Sylphiette, a girl his age who quickly becomes his closest friend. As Rudeus' second chance at life begins, he tries to make the most of his new opportunity while conquering his traumatic past. And perhaps, one day, he may find the one thing he could not find in his old world—love.",userId="3", categoriesId="2", )
@@ -17,8 +17,8 @@ def seed_users():
 
 def undo_posts():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.posts RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
+        db.session.execute(text("DELETE FROM posts"))
 
     db.session.commit()
