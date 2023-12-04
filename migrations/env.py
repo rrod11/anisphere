7ed7544a9f0898aeb9f3e39111d2a271f1c86f44
Roderick <Roderickj800@gmail.com>
2023-12-04 17:48:18 -0500

    crossing my fingers 2x

--- migrations/env.py
+++ migrations/env.py
@@ -115,7 +115,7 @@ def run_migrations_online():
             connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
 
         with context.begin_transaction():
-             if environment == "production":
+            if environment == "production":
                 context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
