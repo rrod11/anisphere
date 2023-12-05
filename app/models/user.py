84ba2432f968ebf2bf28bf66b3f7c6ad52149fd1
Roderick <Roderickj800@gmail.com>
2023-12-05 11:59:43 -0500

    i HATE IT HERE

--- app/models/user.py
+++ app/models/user.py
@@ -43,8 +43,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-# class User(db.Model, UserMixin):
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     if environment == "production":
