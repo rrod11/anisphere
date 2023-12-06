7ad17ab005fdaca26df9c6347867204b1ac621f8
Roderick <Roderickj800@gmail.com>
2023-12-05 20:40:43 -0500

    fixed seed init

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -20,7 +20,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        # undo_postcategories()
+        undo_postcategories()
         undo_reviews()
         undo_posts()
         undo_categories()
@@ -29,14 +29,14 @@ def seed():
     seed_categories()
     seed_posts()
     seed_reviews()
-    # seed_postcategories()
+    seed_postcategories()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    # undo_postcategories()
+    undo_postcategories()
     undo_reviews()
     undo_posts()
     undo_categories()
