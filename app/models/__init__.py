589f0f0b4769f4804661ff4a8fcbaca49b080a31
Roderick <Roderickj800@gmail.com>
2023-12-10 17:27:30 -0500

    i hate it here

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,5 +6,5 @@ from .user import User
 from .post import Post
 from .review import Review
 from .category import Category
-from .post_images import PostImage
+# from .post_images import PostImage
 # from .postCategory import postcategories
