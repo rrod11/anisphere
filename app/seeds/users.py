3cb23c69b3b2faf3f10826384dad8ea879b0df11
Roderick <Roderickj800@gmail.com>
2023-12-17 17:34:12 -0500

    16 users

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,37 +1,3 @@
-# from app.models import db, User, environment, SCHEMA
-# from sqlalchemy.sql import text
-
-
-# # Adds a demo user, you can add other users here if you want
-# def seed_users():
-#     demo = User(
-#         username='Demo', email='demo@aa.io', password='password')
-#     marnie = User(
-#         username='marnie', email='marnie@aa.io', password='password')
-#     bobbie = User(
-#         username='bobbie', email='bobbie@aa.io', password='password')
-
-#     db.session.add(demo)
-#     db.session.add(marnie)
-#     db.session.add(bobbie)
-#     db.session.commit()
-
-
-# # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# # have a built in function to do this. With postgres in production TRUNCATE
-# # removes all the data from the table, and RESET IDENTITY resets the auto
-# # incrementing primary key, CASCADE deletes any dependent entities.  With
-# # sqlite3 in development you need to instead use DELETE to remove all data and
-# # it will reset the primary keys for you as well.
-# def undo_users():
-#     if environment == "production":
-#         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
-#     else:
-#         db.session.execute(text("DELETE FROM users"))
-
-#     db.session.commit()
-
-
 from app.models import db, User, environment, SCHEMA
 from sqlalchemy.sql import text
 
@@ -58,9 +24,21 @@ def seed_users():
         username='Charmy', email='charmy@aa.io', first_name="Charmy", last_name="Pappitson", admin_key="roderick0318",password='charmypassword')
     luck = User(
         username='Luck', email='luck@aa.io', first_name="Luck", last_name="Voltia", admin_key="roderick0318", password='luckpassword')
+    nico = User(
+        username='Nico', email='nico@aa.io', first_name="Nico", last_name="Robin", admin_key="roderick0318", password='nicopassword')
+    nami = User(
+        username='Nami', email='nami@aa.io', first_name="Nami", last_name="Okamura", admin_key="roderick0318", password='namipassword')
+    brook = User(
+        username='Brook', email='brook@aa.io', first_name="Humming", last_name="Brook", admin_key="roderick0318", password='brookpassword')
+    sanji = User(
+        username='Sanji', email='sanji@aa.io', first_name="Sanji", last_name="Germa", admin_key="roderick0318", password='sanjipassword')
+    zoro = User(
+        username='Zoro', email='zoro@aa.io', first_name="Roronoa", last_name="Zoro", admin_key="roderick0318", password='zoropassword')
+    luffy = User(
+        username='Luffy', email='luffy@aa.io', first_name="Monkey D", last_name="Luffy", admin_key="roderick0318", password='luffypassword')
 
 
-    all_users = [asta, yuno, yami, demo, noelle, mimosa, mereoleona, jack, charmy, luck]
+    all_users = [asta, yuno, yami, demo, noelle, mimosa, mereoleona, jack, charmy, luck, nico, nami, brook, sanji, zoro, luffy]
     add_users = [db.session.add(user) for user in all_users]
     db.session.commit()
 
