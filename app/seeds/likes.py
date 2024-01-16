b30ef4c3bed5addd36b9aa1ac7269c6bf5a75a31
Roderick <Roderickj800@gmail.com>
2024-01-16 11:14:47 -0500

    lets see this

--- app/seeds/likes.py
+++ app/seeds/likes.py
@@ -9,16 +9,16 @@ likeChoice= [False, True]
 
 # Adds a seed_likes
 def seed_likes():
-    seed_likes1= Like(likes=True, user_id=randint(1, 10), post_id=randint(1, 25))
-    seed_likes2 = Like(likes=True, user_id=randint(1, 10), post_id=randint(1, 25))
-    seed_likes3 = Like(likes=True, user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes4 = Like(likes=True, user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes5 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes6 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes7 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes8 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes9 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
-    seed_likes10 = Like(likes=choice(likeChoice), user_id=randint(1, 10),post_id=randint(1, 25))
+    seed_likes1= Like(likes=True, user_id=1, post_id=randint(1, 25))
+    seed_likes2 = Like(likes=True, user_id=2, post_id=randint(1, 25))
+    seed_likes3 = Like(likes=True, user_id=3,post_id=randint(1, 25))
+    seed_likes4 = Like(likes=True, user_id=4,post_id=randint(1, 25))
+    seed_likes5 = Like(likes=choice(likeChoice), user_id=5,post_id=randint(1, 25))
+    seed_likes6 = Like(likes=choice(likeChoice), user_id=6,post_id=randint(1, 25))
+    seed_likes7 = Like(likes=choice(likeChoice), user_id=7,post_id=randint(1, 25))
+    seed_likes8 = Like(likes=choice(likeChoice), user_id=8,post_id=randint(1, 25))
+    seed_likes9 = Like(likes=choice(likeChoice), user_id=9,post_id=randint(1, 25))
+    seed_likes10 = Like(likes=choice(likeChoice), user_id=10,post_id=randint(1, 25))
 
 
     all_likes = [seed_likes1, seed_likes2, seed_likes3, seed_likes4,
