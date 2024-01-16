9d42aed796318ae6b398840d4ab2775e8c72888c
Roderick <Roderickj800@gmail.com>
2024-01-16 10:38:23 -0500

    now i just have to reseed

--- app/seeds/dislikes.py
+++ app/seeds/dislikes.py
@@ -9,16 +9,16 @@ likeChoice= [False, True]
 
 # Adds a seed_dislikes
 def seed_dislikes():
-    seed_dislikes1= Dislike(dislikes=True, user_id=1, post_id=randint(1, 122))
-    seed_dislikes2 = Dislike(dislikes=True, user_id=2, post_id=randint(1, 122))
-    seed_dislikes3 = Dislike(dislikes=True, user_id=3,post_id=randint(1, 122))
-    seed_dislikes4 = Dislike(dislikes=True, user_id=4,post_id=randint(1, 122))
-    seed_dislikes5 = Dislike(dislikes=choice(likeChoice), user_id=5,post_id=randint(1, 122))
-    seed_dislikes6 = Dislike(dislikes=choice(likeChoice), user_id=6,post_id=randint(1, 122))
-    seed_dislikes7 = Dislike(dislikes=choice(likeChoice), user_id=7,post_id=randint(1, 122))
-    seed_dislikes8 = Dislike(dislikes=choice(likeChoice), user_id=8,post_id=randint(1, 122))
-    seed_dislikes9 = Dislike(dislikes=choice(likeChoice), user_id=9,post_id=randint(1, 122))
-    seed_dislikes10 = Dislike(dislikes=choice(likeChoice), user_id=10,post_id=randint(1, 122))
+    seed_dislikes1= Dislike(dislikes=True, user_id=1, post_id=randint(1, 25))
+    seed_dislikes2 = Dislike(dislikes=True, user_id=2, post_id=randint(1, 25))
+    seed_dislikes3 = Dislike(dislikes=True, user_id=3,post_id=randint(1, 25))
+    seed_dislikes4 = Dislike(dislikes=True, user_id=4,post_id=randint(1, 25))
+    seed_dislikes5 = Dislike(dislikes=choice(likeChoice), user_id=5,post_id=randint(1, 25))
+    seed_dislikes6 = Dislike(dislikes=choice(likeChoice), user_id=6,post_id=randint(1, 25))
+    seed_dislikes7 = Dislike(dislikes=choice(likeChoice), user_id=7,post_id=randint(1, 25))
+    seed_dislikes8 = Dislike(dislikes=choice(likeChoice), user_id=8,post_id=randint(1, 25))
+    seed_dislikes9 = Dislike(dislikes=choice(likeChoice), user_id=9,post_id=randint(1, 25))
+    seed_dislikes10 = Dislike(dislikes=choice(likeChoice), user_id=10,post_id=randint(1, 25))
 
 
     all_dislikes = [seed_dislikes1, seed_dislikes2, seed_dislikes3, seed_dislikes4,
